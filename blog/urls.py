7a749df7a2d3ce95bda4bbb2bfc8d206130664a0
KurtVobain <one1two215@gmail.com>
2019-12-01 00:58:40 +0300

    had been added html-page login

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('auth/', views.auth, name='auth')
 ]
